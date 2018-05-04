467d082d6abc58cf5b1d38126bd8d59de987245f
Andrei Borissenko <Andrei.Borissenko@gmail.com>
2018-05-04 13:33:48 -0400

    Dynamic import example (commented out), basic component usage

--- src/App.js
+++ src/App.js
@@ -2,13 +2,28 @@ import React, { Component } from 'react'
 import logo from './logo.svg'
 import './App.css'
 
+const welcome = 'Welcome to React!'
+
+class Welcome extends Component {
+  render() {
+    return <h1 className="App-title">{welcome}</h1>
+  }
+}
+
 class App extends Component {
+  // Dynamic Import code splitting example
+  // async handleClick() {
+  //   const { moduleA } = await import("./moduleA");
+
+  //   console.log(moduleA);
+  // }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React!</h1>
+          <Welcome />
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
