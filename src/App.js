033b668fb5f92f5a04ac55353e4288b4639a63b5
Andrei Borissenko <Andrei.Borissenko@gmail.com>
2018-05-03 20:38:04 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+/* eslint-disable no-unused-vars */
+// Need to turn off check for unused vars here since React is required in scope to use JSX
+import React, { Component } from 'react'
+/* eslint-enable */
+import logo from './logo.svg'
+import './App.css'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React!</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    )
+  }
+}
+
+export default App
