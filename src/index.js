92b18d7ab6533dc6b84d08746bd29a69e748ac56
Andrei Borissenko <Andrei.Borissenko@gmail.com>
2018-05-07 09:58:31 -0400

    Reset to level up tutorial

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
-import FilterableProductTable from './FilterableProductTable'
+// import FilterableProductTable from './FilterableProductTable'
 import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render( <FilterableProductTable />, document.getElementById( 'root' ) )
+ReactDOM.render( <App />, document.getElementById( 'root' ) )
 registerServiceWorker()
