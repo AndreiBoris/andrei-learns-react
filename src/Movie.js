3ff85a7ee23628655b66aa2f924e6f603c27071b
Andrei Borissenko <Andrei.Borissenko@gmail.com>
2018-05-08 11:14:04 -0400

    Switch to eslintrc.js from Scott Tulinsky

--- src/Movie.js
+++ src/Movie.js
@@ -1,19 +1,19 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export default class Movie extends Component {
   static propTypes = {
-    movie: PropTypes.shape( {
+    movie: PropTypes.shape({
       title: PropTypes.string.isRequired,
-    } ).isRequired,
-  }
+    }).isRequired,
+  };
 
   render() {
-    const { movie } = this.props
+    const { movie } = this.props;
     return (
       <div>
         <h3>{movie.title}</h3>
       </div>
-    )
+    );
   }
 }
