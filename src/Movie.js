723b444b58526edb4b86140d6fea3f31e054488f
Andrei Borissenko <Andrei.Borissenko@gmail.com>
2018-05-09 14:28:33 -0400

    Add a Poster styled component over img, add dark background

--- src/Movie.js
+++ src/Movie.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 
 const POSTER_PATH = 'http://image.tmdb.org/t/p/w154';
 
+const Poster = styled.img`
+  box-shadow: 0 0 35px black;
+`;
+
 const Movie = ({ movie }) => (
-  <div>
-    <Link to={`/${movie.id}`}>
-      <img src={`${POSTER_PATH}${movie.poster_path}`} alt={movie.title} />
-    </Link>
-  </div>
+  <Link to={`/${movie.id}`}>
+    <Poster src={`${POSTER_PATH}${movie.poster_path}`} alt={movie.title} />
+  </Link>
 );
 
 Movie.propTypes = {
